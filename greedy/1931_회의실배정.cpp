1735122e65110416c7d7cefef5d084b4680ae625
curio <aksdmj@gmail.com>
2018-02-10 06:00:09 +0900

    1931

---
+++ greedy/1931_회의실배정.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <utility>
+#include <
+using namespace std;
+
+
+int n;
+pair<int, int> arr[100010];
+
+
+int main(){
+  scanf("%d",&n);
+  for(int i=0;i<n;i++){
+    scnaf("%d %d",&arr[i].first,&arr[i].second);
+  }
+
+}
